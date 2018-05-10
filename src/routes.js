3136ab34d7a31da2d88ca1eca0fe9946e55fff2b
Franck <franck.abgrall@zenika.com>
2018-05-10 11:11:00 +0200

    Route config

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './components/Home';
+
+// Use plugin vue-router
+Vue.use(Router);
+
+// Define routes config
+export default new Router({
+  routes: [{}]
+});
