2ae519e9d2ce1e50abad696a1cd8aef180e1734b
Franck <franck.abgrall@zenika.com>
2018-05-10 11:40:28 +0200

    router link removed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">Home</router-link>
+      <a class="navbar-brand" href="#">Home</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -8,7 +8,7 @@
       <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <router-link class="nav-link" to="/favorites">Favorites</router-link>
+            <a class="nav-link" href="#">Favorites</a>
           </li>
         </ul>
         <form class="form-inline">
